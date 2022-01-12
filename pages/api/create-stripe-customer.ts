19dd39270fc5cf97a2fc55e4eaac791ad27ad5de
sfj.eth <0x0xsfj@gmail.com>
2022-01-11 23:09:57 -0800

    Install Stripe

---
+++ pages/api/create-stripe-customer.ts
@@ -0,0 +1,11 @@
+import initStripe from 'stripe';
+const handler = async (req, res) => {
+  const stripe = initStripe(process.env.STRIPE_SECRET_KEY);
+
+  const customer = await stripe.customers.create({
+    email: req.body.record.email,
+  });
+  res.send({ message: `Stripe customer created ${customer.id}` });
+};
+
+export default handler;
