106c07fa2b3f51625686126ee4749e3926135b41
sfj.eth <0x0xsfj@gmail.com>
2022-01-16 23:13:00 -0800

    Billing auth secret

--- pages/api/create-stripe-customer.ts
+++ pages/api/create-stripe-customer.ts
@@ -1,6 +1,22 @@
 import initStripe from 'stripe';
 import { supabase } from '../../utils/supabase';
+
 const handler = async (req, res) => {
+  if (!req.body.record.email || !req.body.record.id) {
+    res.status(401).send({
+      error: `Email and Id is required only returned: ${req.body.record.email ? `Email: ${req.body.record.email}` : ''}${req.body.record.id ? `Id: ${req.body.record.email}` : ''}`,
+    });
+
+    return;
+  }
+
+  if (req.query.API_ROUTE_SECRET !== process.env.API_ROUTE_SECRET) {
+    console.log('API Route Secret does not match');
+    console.log(`API Route Secret: ${req.query.API_ROUTE_SECRET}`);
+    res.status(401).send('You are unauthorized to access this api.');
+    return;
+  }
+
   const stripe = new initStripe(process.env.STRIPE_SECRET_KEY as string);
 
   const customer = await stripe.customers.create({
--- utils/supabase.ts
+++ utils/supabase.ts
@@ -1,8 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
-console.log(supabaseUrl);
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
-console.log(supabaseAnonKey);
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
