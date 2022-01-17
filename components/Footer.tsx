0da6781fd9c043496c8539e44f6d273ec621721b
sfj.eth <0x0xsfj@gmail.com>
2022-01-16 23:12:26 -0800

    Footer Date

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,4 +1,4 @@
-import { Box, Stack } from '@chakra-ui/react';
+import { Box, Stack, Text } from '@chakra-ui/react';
 import * as React from 'react';
 // import { Copyright } from './Copyright';
 import { Logo } from './Logo';
@@ -8,7 +8,10 @@ const Footer = () => (
   <Box as="footer" role="contentinfo" mx="auto" maxW="7xl" py="12" px={{ base: '4', md: '8' }}>
     <Stack>
       <Stack direction="row" spacing="4" align="center" justify="space-between">
-        <Logo />
+        <Text fontSize="sm" fontWeight="bold" color="gray.500">
+          © {new Date().getFullYear()}
+        </Text>
+        {/* <Logo /> */}
         {/* <SocialMediaLinks /> */}
       </Stack>
       {/* <Copyright alignSelf={{ base: 'center', sm: 'start' }} /> */}
